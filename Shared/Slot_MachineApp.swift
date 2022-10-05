c7512c4e7affbebc7ddb94786d296a23af889c4e
andres2392 <32281805+andres2392@users.noreply.github.com>
2022-10-05 15:21:02 -0400

    Initial Commit

---
+++ Shared/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Shared
+//
+//  Created by PDWS on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
